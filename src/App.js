d3ec5898773e4fe851002625e1457535008bd2b6
Debora Ermel <debbyermel@gmail.com>
2020-11-10 17:55:00 -0600

    Added home/about/shop components

--- src/App.js
+++ src/App.js
@@ -1,53 +1,25 @@
 import React, {useEffect, useState} from 'react';
+import About from './About';
+import Home from './Home';
+import Nav from './Nav';
 import Recipe from './Recipe';
+import Shop from './Shop';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
-  const APP_ID = 'c00f1910';
-  const APP_KEY = '006bba617a6c553a45d20ee23acb76f3';
-
-  const[recipes, setRecipes] = useState([]);
-  const[search, setSearch] = useState('');
-  const[query, setQuery] = useState('chicken');
-
-  useEffect(() => {
-    getRecipes();
-  }, [query]);
-
-  const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-    const data = await response.json();
-    setRecipes(data.hits);
-  }
-
-  const updateSearch = e => {
-    setSearch(e.target.value);
-  }
-
-  const getSearch = e => {
-    e.preventDefault();
-    setQuery(search);
-    setSearch('');
-  }
-
   return(
-    <div className="app">
-      <form onSubmit={getSearch} className="search-form">
-        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
-        <button className="search-button" type="submit">Search</button>
-      </form>
-      <div className="recipes-list">
-        {recipes.map(recipe =>(
-          <Recipe
-          key={recipe.recipe.label}
-          title={recipe.recipe.label}
-          image={recipe.recipe.image}
-          calories={recipe.recipe.calories}
-          ingredients={recipe.recipe.ingredients}
-          />
-        ))}
+    <Router>
+      <div className="app">
+        <Nav />
+        <Switch>
+          < Route path="/" exact component={Home} />
+          < Route path="/about" component={About} />
+          < Route path="/shop" component={Shop} />
+          < Route path="/recipe" component={Recipe} />
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
